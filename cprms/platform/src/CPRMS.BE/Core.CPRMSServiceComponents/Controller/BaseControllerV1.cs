4dd948a09d066ea97e2e040cc783fcabc78e88d7
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2025-05-08 22:11:47 +0700

    [CPRMS] Config login Google

--- cprms/platform/src/CPRMS.BE/Core.CPRMSServiceComponents/Controller/BaseControllerV1.cs
+++ cprms/platform/src/CPRMS.BE/Core.CPRMSServiceComponents/Controller/BaseControllerV1.cs
@@ -2,9 +2,18 @@
 namespace Core.CPRMSServiceComponents.Controller
 {
     [ApiController]
-    [Route("api/{appId:GUID}/[controller]")]
+    [Route("api/{appId:guid}/[controller]")]
     public class BaseControllerV1 : ControllerBase
     {
-        protected Guid AppId => Guid.Parse((string)RouteData.Values["appId"]);
+        protected Guid AppId
+        {
+            get
+            {
+                if (Guid.TryParse((string)RouteData.Values["appId"], out var id))
+                    return id;
+                throw new ArgumentException("Invalid appId");
+            }
+        }
+
     }
 }
