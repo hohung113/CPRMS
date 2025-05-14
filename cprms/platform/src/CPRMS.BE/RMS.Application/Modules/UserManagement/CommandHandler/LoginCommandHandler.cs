40723aa3208daa5470da87251e1d5665c3ba6669
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2025-05-15 00:02:47 +0700

    [CPRMS-0000] Add Tempalate Send Request

---
+++ cprms/platform/src/CPRMS.BE/RMS.Application/Modules/UserManagement/CommandHandler/LoginCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Rms.Application.Modules.UserManagement.Command;
+using Rms.Domain.Modules.UserSystem.Interface;
+
+namespace Rms.Application.Modules.UserManagement.CommandHandler
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, bool>
+    {
+        private readonly IUserRepository _userRepository;
+        public LoginCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public Task<bool> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            // Implement Here 
+            throw new NotImplementedException();
+        }
+    }
+}
